7ed3280bbf45d75f7d3fd7f3ec84eedf4c288eac
Robin <robalexclark@hotmail.com>
2025-07-31 11:07:47 +0100

    Create solution and add in Scalar page

---
+++ ENSEKTask/ENSEKTask/Program.cs
@@ -0,0 +1,31 @@
+﻿using Scalar.AspNetCore;
+
+namespace ENSEKTask
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Services
+            builder.Services.AddControllers();
+            builder.Services.AddOpenApi();
+
+            var app = builder.Build();
+
+            // HTTP pipeline
+            if (app.Environment.IsDevelopment())
+            {
+                app.MapOpenApi();
+                app.MapScalarApiReference();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
